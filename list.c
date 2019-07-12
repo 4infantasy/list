209d8fc0d3081258c737d60d70c322198fcdfddc
4infantasy <4infantasy@gmail.com>
2019-07-12 09:02:12 +0800

    创建list.c

---
+++ list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct list_node {
+
+	struct list_node *next;
+	void   *val;
+
+} list_node_t ;
+
+
+list_node_t 
+
+
+int main(int argc, char const *argv[])
+{
+	printf("list test.\n");
+
+	return 0;
+}
